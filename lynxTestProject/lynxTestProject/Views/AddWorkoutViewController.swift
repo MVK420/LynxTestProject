edfa4f4d75d7ba33144359136f2c9d038932a30a
Mozes <mozes.vidami@appforte.ro>
2021-07-10 12:38:56 +0300

    FrameExtractor implemented

--- lynxTestProject/lynxTestProject/Views/AddWorkoutViewController.swift
+++ lynxTestProject/lynxTestProject/Views/AddWorkoutViewController.swift
@@ -7,14 +7,23 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
-class AddWorkoutViewController: UIViewController {
+class AddWorkoutViewController: BaseViewController {
     @IBOutlet private weak var nameTextField: UITextField!
     @IBOutlet private weak var caloriesTextField: UITextField!
     @IBOutlet private weak var workoutImageView: UIImageView!
     @IBOutlet private weak var dateOfWorkoutDatePicker: UIDatePicker!
     @IBOutlet private weak var durationTextField: UITextField!
-    @IBOutlet private weak var cameraButton: UIButton!
+    @IBOutlet private weak var cameraButton: UIButton! {
+        didSet {
+            self.cameraButton.rx.tap.bind {
+                self.presentViewController(with: ViewControllers.camera.rawValue)
+            }.disposed(by: disposeBag)
+        }
+    }
     @IBOutlet private weak var galleryButton: UIButton!
     @IBOutlet private weak var saveButton: UIButton!
+    private let disposeBag: DisposeBag = DisposeBag()
 }
--- lynxTestProject/lynxTestProject/Views/CameraViewController.swift
+++ lynxTestProject/lynxTestProject/Views/CameraViewController.swift
@@ -11,4 +11,17 @@ import UIKit
 class CameraViewController: UIViewController {
     @IBOutlet weak var captureButton: UIButton!
     @IBOutlet weak var cameraImageView: UIImageView!
+    private var frameExtractor: FrameExtractor!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.frameExtractor = FrameExtractor()
+        self.frameExtractor.delegate = self
+    }
+}
+
+extension CameraViewController: FrameExtractorDelegate {
+    func captured(image: UIImage) {
+        cameraImageView.image = image
+    }
 }
